7ccecff8075959164ed86b8d78ad395c5487dca1
Joe Fleming <joe.fleming@gmail.com>
2017-12-05 19:56:56 -0700

    feat: generate barebones package

---
+++ sao.js
@@ -0,0 +1,33 @@
+module.exports = {
+  enforceNewFolder: true,
+  prompts: {
+    package: {
+      required: true,
+      message: 'What is the name of the new package?',
+    },
+    description: {
+      message: 'How would you describe the new package?',
+      default: 'Another fine package',
+    },
+    name: {
+      message: 'What is your name?',
+      default: ':gitUser:',
+      store: true,
+    },
+    username: {
+      message: 'What is your GitHub username?',
+      default: ':gitUser:',
+      store: true,
+    },
+    repository: {
+      message: 'What repository path should be used?',
+    },
+    public: {
+      type: 'confirm',
+      message: 'Is this package public?',
+      default: true,
+    },
+  },
+  gitInit: true,
+  installDependencies: false,
+};
---
+++ template/README.md
@@ -0,0 +1,9 @@
+# <%= package %>
+
+<%= description %>.
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+#### License
+
+MIT © [<%= username %>](<%= `https://github.com/${username}` %>)
